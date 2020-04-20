63ccd672ede2e62c0c6fdbe504624ea67fb09849
Constantin Dimitrenco <constantin.dimitrenco@gmail.com>
2020-04-20 11:18:06 +0100

    first steps

--- script.js
+++ script.js
@@ -6,7 +6,20 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
-}
+  rootElem.className = "root";
 
+  episodeList.forEach((element) => {
+    var tile = document.createElement("section");
+    tile.className = "tile";
+    rootElem.appendChild(tile);
+    var id = document.createElement("h1");
+    tile.appendChild(id);
+    id.textContent = `Id:${element.id}`;
+    id.className = "id";
+    var episName = document.createElement("h1");
+    tile.appendChild(episName);
+    episName.textContent = `Id:${element.name}`;
+  });
+  console.log(rootElem);
+}
 window.onload = setup;
