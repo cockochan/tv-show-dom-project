43a49c5dee2e3c40c20f0043cf37a0074866780a
cockochan <56898295+cockochan@users.noreply.github.com>
2020-04-23 18:46:59 +0300

    da

--- script.js
+++ script.js
@@ -8,31 +8,6 @@ function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.className = "root";
 
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-  episodeList.forEach((element) => {
-    var tile = document.createElement("section");
-    tile.className = "tile";
-    rootElem.appendChild(tile);
-    
-=======
-
-  var nav = document.createElement("nav");
-  nav.className = "nav";
-  rootElem.appendChild(nav);
-
-  var filEpisodeList = episodeList;
- 
-
-  let build = function(filEpisodeList){
-    filEpisodeList.forEach((element) => {
-      console.log(filEpisodeList);
-    var tile = document.createElement("section");
-    tile.className = "tile";
-    series.appendChild(tile);
-
->>>>>>> Stashed changes
-=======
 
   var nav = document.createElement("nav");
   nav.className = "nav";
@@ -53,7 +28,6 @@ function makePageForEpisodes(episodeList) {
     tile.className = "tile";
     rootElem.appendChild(tile);
 
->>>>>>> new
     var season = document.createElement("h1");
     tile.appendChild(season);
     season.className = "season";
@@ -97,47 +71,22 @@ function makePageForEpisodes(episodeList) {
     img.src = element.image.medium;
     img.className = "img";
   });
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-  console.log(rootElem);
-=======
 };
-var series= document.createElement("div");
-series.className = "series";
-rootElem.appendChild(series);
-
-
-build(filEpisodeList);
-=======
-};
->>>>>>> new
 let searchBox  = document.createElement("INPUT");
 searchBox.setAttribute("type", "search");
 searchBox.className = "searchBox ";
 nav.appendChild(searchBox);
-<<<<<<< HEAD
-// let filEpisodeList = [];
-=======
 let filEpisodeList = [];
->>>>>>> new
 searchBox.addEventListener('keyup', event => {
   // get the current searchBox value
   const searchValue = event.target.value
 
   // then do something with it
   filEpisodeList = episodeList.filter(el=>el.summary.includes(searchValue));
-<<<<<<< HEAD
-  series.textContent = "";
-=======
 
->>>>>>> new
   build(filEpisodeList);
   console.log(searchValue);
 
 });
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> new
 }
 window.onload = setup;
