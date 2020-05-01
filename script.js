9de8be20117cb25293339435b0442873e690f991
Constantin Dimitrenco <56898295+cockochan@users.noreply.github.com>
2020-05-01 22:23:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 //You can edit ALL of the code here
 function setup() {
-<<<<<<< Updated upstream
 
   
 let episodeList;
@@ -18,39 +17,6 @@ console.log(episodeList);
 };
 // const allEpisodes = getAllEpisodes();
 makePageForEpisodes(episodeList);
-=======
-  // const allEpisodes = getAllEpisodes();
-  // makePageForEpisodes(allEpisodes);
-  
-    fetch(`https://api.tvmaze.com/shows/1/episodes`)
-    .then(response => response.json())
-    .then(allEpisodes =>makePageForEpisodes(allEpisodes));
-};
-let refetch = function(showId){
-  
-  fetch(`https://api.tvmaze.com/shows/${showId}/episodes`)
-  .then(response => response.json())
-  .then(allEpisodes =>makePageForEpisodes(allEpisodes));
-};
-const allShows = getAllShows();
-  //   if (x>y){
-  //     return 1;
-  //   } else if (x<y) {
-  //     return -1;
-  //   }
-  //   return 0;
-//   // });
-let sortedAllShows = allShows.sort(function(x,y){
-  if (x.name>y.name){
-        return 1;
-      } else if (x<y) {
-        return -1;
-      }
-      return 0;
-    });
-    
-
->>>>>>> Stashed changes
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.className = "root";
@@ -68,8 +34,6 @@ function makePageForEpisodes(episodeList) {
   dropDownContainer.appendChild(dropDown);
   dropDown.className="dropDown";
 
-<<<<<<< Updated upstream
-=======
   //show selector
   // create dropdown list
   var dropDownShowsContainer = document.createElement("div");
@@ -93,7 +57,6 @@ sortedAllShows.forEach(show =>addShowToSelector(show)
   // display.textContent = `showing ${filEpisodeList.length} episodes`;
 );
 // };
->>>>>>> Stashed changes
  
 // for each stuff
   let build = function(filEpisodeList){
